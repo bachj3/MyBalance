93270a37ee5b962192005ba3ec94b703cabf42cd
bachj3 <bachj3@bfh.ch>
2014-05-20 11:47:06 +0200

    balance

--- js/page_balance.js
+++ js/page_balance.js
@@ -1,11 +1,42 @@
+// Bei einem Klick auf das Bild, wird das Bild geändert und die Slider entsprechend angepasst
+
 document.getElementById('balance_image').addEventListener('click', diffImage, false);
+
 function diffImage() {
     var img = document.getElementById("balance_image");
+
     if (img.src.match(/unbalanced/)) {
-        console.log('balanced');
+
         img.src = "img/icn/balanced.png";
+
+        // Slider wird initialisiert
+        $('.slider').slider();
+
+        // Value des Sliders wird auf 5 gesetzt
+        $('#slider_food').val("5");
+        // Der Slider wird refreshed um den neuen Wert anzuzeigen
+        $('#slider_food').slider('refresh');
+        
+        $('#slider_activity').val("5");
+        $('#slider_activity').slider('refresh');
+        
+        $('#slider_relaxation').val("5");
+        $('#slider_relaxation').slider('refresh');
+
     } else {
-        console.log('unbalanced');
+
         img.src = "img/icn/unbalanced.png";
+
+        $('.slider').slider();
+
+        $('#slider_food').val("6");
+        $('#slider_food').slider('refresh');
+        
+        $('#slider_activity').val("7");
+        $('#slider_activity').slider('refresh');
+        
+        $('#slider_relaxation').val("7");
+        $('#slider_relaxation').slider('refresh');
+
     }
 }
