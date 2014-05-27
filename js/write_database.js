c544913ce5445deea618e8026c31717f6ea18c7b
bachj3 <bachj3@bfh.ch>
2014-05-27 13:30:29 +0200

    zurück

--- js/write_database.js
+++ js/write_database.js
@@ -1,4 +1,5 @@
 console.log("script write_database");
+
 var db = null;
 
 var IdCategory = ["3", "3", "3", "3", "1", "2", "4"];
@@ -6,49 +7,39 @@ var IdProgram = ["2", "2", "1", "1", "100", "200", "400"];
 var P_Name = ["Fussprogramm", "Fussprogramm", "Kr&auml;ftigung Hand", "Kr&auml;ftigung Hand", "Bewegungsprogramm", "Entspannungsprogramm", "Eigenes Programm"];
 var IdExercise = ["7", "8", "11", "12", "101", "201", "401"];
 var E_Name = ["Dorsalflexion", "Eversion", "Hand Dorsalextension", "Hand Kneifkraft", "Schwimmen", "Lesen", "Meine Übung"];
-var E_Description = [
-"Setzen Sie Ihren gesunden Fuss auf den Fussr&uuml;cken Ihres verletzen Fusses und erzeugen Sie dabei eine entsprechende Spannung auf den Vorfuss.", 
-"Ein doppeltes Deuserband kommt &uuml;ber beide Fussr&uuml;cken. Bewegen Sie Ihr verletztes Bein maximal nach aussen, so dass das Deuserband maximal gespannt wird. Dann wird nur die Ferse auf den Boden gestellt.", 
-"Ellbogen 90&deg; gebeugt, nach aussen gedrehter Arm. In dieser Position liegt der Handr&uuml;cken auf der Waage. Mit der Hand kr&auml;ftig das Gewicht auf die Waage dr&uuml;cken. Die Spannung halten und die &Uuml;bung wiederholen.", 
-"Kneten Sie den Therapieball.", 
-"Schwimmen bietet eine ideale Kombination von Ausdauer, Muskelaufbau und Fettverbrennung.",
-"Lesen Sie doch wieder einmal ein Buch",
-"-"];
+var E_Description = ["Setzen Sie Ihren gesunden Fuss auf den Fussr&uuml;cken Ihres verletzen Fusses und erzeugen Sie dabei eine entsprechende Spannung auf den Vorfuss.", "Ein doppeltes Deuserband kommt &uuml;ber beide Fussr&uuml;cken. Bewegen Sie Ihr verletztes Bein maximal nach aussen, so dass das Deuserband maximal gespannt wird. Dann wird nur die Ferse auf den Boden gestellt.", "Ellbogen 90&deg; gebeugt, nach aussen gedrehter Arm. In dieser Position liegt der Handr&uuml;cken auf der Waage. Mit der Hand kr&auml;ftig das Gewicht auf die Waage dr&uuml;cken. Die Spannung halten und die &Uuml;bung wiederholen.", "Kneten Sie den Therapieball.", "Schwimmen bietet eine ideale Kombination von Ausdauer, Muskelaufbau und Fettverbrennung.", "Lesen Sie doch wieder einmal ein Buch", "-"];
 var S = ["2", "3", "2", "3", "-", "-", "-"];
 var R = ["12", "8", "4", "20", "-", "-", "-"];
 var W = ["-", "-", "1.5", "-", "-", "-", "-"];
 var D = ["-", "-", "00:00:10", "00:00:15", "00:30:00", "00:30:00", "00:30:00"];
 var Re = ["00:00:30", "00:00:30", "00:00:30", "00:00:10", "-", "00:15:00", "00:00:00"];
-var Ref = ["Fuss_Dorsalflexion_Anspannungsuebung.png", "Fuss_Eversion_Sitz.png", "Hand_Hand_Dorsalextension.png", "Hand_Hand_Kneifkraft.png", "Schwimmen.png", "Lesen.png", "Camera.png" ];
+var Ref = ["Fuss_Dorsalflexion_Anspannungsuebung.png", "Fuss_Eversion_Sitz.png", "Hand_Hand_Dorsalextension.png", "Hand_Hand_Kneifkraft.png", "Schwimmen.png", "Lesen.png", "Camera.png"];
 
-// document.getElementById("refresh").addEventListener('click', function() {
-    // db.transaction(deleteTable, errorCB, successCB);
-    // db.transaction(writeExercises, errorCB, successCB);
-    // // deleteDatabase("Physio.db");
-    // alert("table filled");
-// 
-// });
-
+// wartet darauf, dass das Gerät bereit ist
 document.addEventListener("deviceready", function() {
+    // öffnet die Datenbank Physio
     db = window.openDatabase("Physio", "1.0", "physio", 2000000);
     console.log("deviceready_write_database");
+    // löscht die Tabelle
     db.transaction(deleteTable, errorCB, successCB);
+    // schreibt die Datensätze
     db.transaction(writeExercises, errorCB, successCB);
     console.log("table filled");
 }, false);
 
-
+// löscht die Tabelle Program
 function deleteTable(tx) {
     tx.executeSql('DROP TABLE IF EXISTS Program');
 
 }
 
+// schreibt die Datensätze in die Tabelle Program
 function writeExercises(tx) {
     tx.executeSql('CREATE TABLE IF NOT EXISTS Program(id unique, idCategory, idProgram, P_Name, idExercise, E_Name, E_Description, S, R, W, D, Re, Ref)');
 
+// iteriert durch alle Listenelemente und schreibt sie in die Tabelle
     for (var i = 0; i < IdCategory.length; i++) {
         var index = i + 1;
-        // console.log(index);
         tx.executeSql('INSERT INTO Program(id, idCategory, idProgram, P_Name, idExercise, E_Name, E_Description, S, R, W, D, Re, Ref) VALUES (' + index + ', "' + IdCategory[i] + '", ' + IdProgram[i] + ', "' + P_Name[i] + '", ' + IdExercise[i] + ', "' + E_Name[i] + '", "' + E_Description[i] + '", "' + S[i] + '", "' + R[i] + '", "' + W[i] + '", "' + D[i] + '", "' + Re[i] + '", "' + Ref[i] + '")');
     }
 }
