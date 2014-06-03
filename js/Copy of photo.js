1a1dd430b7dab000229d3be217818c1e541c5fff
bachj3 <bachj3@bfh.ch>
2014-06-03 13:06:52 +0200

    photo

---
+++ js/Copy of photo.js
@@ -0,0 +1,94 @@
+var pictureSource;
+// picture source
+var destinationType;
+// sets the format of returned value
+
+// Wait for PhoneGap to connect with the device
+//
+document.addEventListener("deviceready", onDeviceReady, false);
+
+// PhoneGap is ready to be used!
+//
+function onDeviceReady() {
+    alert("onDeviceReady");
+    console.log("onDeviceReady");
+    alert(navigator.camera.PictureSourceType);
+    alert(navigator.camera.DestinationType);
+    pictureSource = navigator.camera.PictureSourceType;
+    destinationType = navigator.camera.DestinationType;
+    alert(pictureSource);
+    alert(destinationType);
+}
+
+// Called when a photo is successfully retrieved
+//
+// function onPhotoDataSuccess(imageData) {
+    // console.log("onPhotoDataSuccess");
+    // // Uncomment to view the base64 encoded image data
+    // // console.log(imageData);
+    // // alert(imageData);
+// 
+    // // Get image handle
+    // //
+    // var img_selfie = document.getElementById('e_ref');
+// 
+    // // Unhide image elements
+    // //
+    // img_selfie.style.display = 'block';
+// 
+    // // Show the captured photo
+    // // The inline CSS rules are used to resize the image
+    // //
+    // img_selfie.src = "data:image/jpeg;base64," + imageData;
+// }
+
+// Called when a photo is successfully retrieved
+//
+function onPhotoURISuccess(imageURI) {
+    console.log("onPhotoURISuccess");
+    // Uncomment to view the image file URI
+    // console.log(imageURI);
+    // alert(imageURI);
+
+    // Get image handle
+    //
+    var img_selfie = document.getElementById('e_ref');
+
+    // Unhide image elements
+    //
+    img_selfie.style.display = 'block';
+
+    // Show the captured photo
+    // The inline CSS rules are used to resize the image
+    //
+    img_selfie.src = imageURI;
+
+}
+
+// A button will call this function
+//
+// function capturePhoto() {
+    // console.log("capturePhoto");
+    // // Take picture using device camera and retrieve image as base64-encoded string
+    // navigator.camera.getPicture(onPhotoDataSuccess, onFail, {
+        // destinationType : Camera.DestinationType.FILE_URI,
+        // quality : 50
+    // });
+// }
+
+function getPhoto(source) {
+    console.log("getPhoto");
+    // Retrieve image file location from specified source
+    navigator.camera.getPicture(onPhotoURISuccess, onFail, {
+        quality : 50,
+        destinationType : Camera.DestinationType.FILE_URI,
+        sourceType : source
+    });
+}
+
+// Called if something bad happens.
+//
+function onFail(message) {
+    alert('Failed because: ' + message);
+    conole.log('Failed because: ' + message);
+}
