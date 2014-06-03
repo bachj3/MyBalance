f3ae399bc60201ea3e4cf279bef139c44e3b6ca5
bachj3 <bachj3@bfh.ch>
2014-06-03 13:32:31 +0200

    photo.js

--- js/photo.js
+++ js/photo.js
@@ -1,92 +1,31 @@
-var pictureSource;   // picture source
-var destinationType; // sets the format of returned value 
+var pictureSource;
+var destinationType;
 
-// Wait for PhoneGap to connect with the device
-//
-document.addEventListener("deviceready",onDeviceReady,false);
+document.addEventListener("deviceready", onDeviceReady, false);
 
-// PhoneGap is ready to be used!
-//
 function onDeviceReady() {
     console.log("onDeviceReady");
-    pictureSource=navigator.camera.PictureSourceType;
-    destinationType=navigator.camera.DestinationType;
+    pictureSource = navigator.camera.PictureSourceType;
+    destinationType = navigator.camera.DestinationType;
 }
 
-// Called when a photo is successfully retrieved
-//
-function onPhotoDataSuccess(imageData) {
-    console.log("onPhotoDataSuccess");
-  // Uncomment to view the base64 encoded image data
-  // console.log(imageData);
-  // alert(imageData);
-
-  // Get image handle
-  //
-  var img_selfie = document.getElementById('e_ref');
-
-  // Unhide image elements
-  //
-  img_selfie.style.display = 'block';
-
-  // Show the captured photo
-  // The inline CSS rules are used to resize the image
-  //
-  img_selfie.src = "data:image/jpeg;base64," + imageData;
-}
-
-// Called when a photo is successfully retrieved
-//
 function onPhotoURISuccess(imageURI) {
     console.log("onPhotoURISuccess");
-  // Uncomment to view the image file URI 
-  // console.log(imageURI);
-  // alert(imageURI);
+    var img_selfie = document.getElementById('e_ref');
 
-  // Get image handle
-  //
-  var img_selfie = document.getElementById('e_ref');
-
-  // Unhide image elements
-  //
-  img_selfie.style.display = 'block';
-
-  // Show the captured photo
-  // The inline CSS rules are used to resize the image
-  //
-  img_selfie.src = imageURI;
-
+    img_selfie.style.display = 'block';
+    img_selfie.src = imageURI;
 }
 
-// A button will call this function
-//
-function capturePhoto() {
-    console.log("capturePhoto");
-  // Take picture using device camera and retrieve image as base64-encoded string
-  navigator.camera.getPicture(onPhotoDataSuccess, onFail, {destinationType: Camera.DestinationType.FILE_URI, quality: 50 });
-}
-
-// A button will call this function
-//
-function capturePhotoEdit() {
-    console.log("capturePhotoEdit");
-  // Take picture using device camera, allow edit, and retrieve image as base64-encoded string  
-  navigator.camera.getPicture(onPhotoDataSuccess, onFail, { destinationType: Camera.DestinationType.FILE_URI,quality: 20, allowEdit: true }); 
-}
-
-// A button will call this function
-//
 function getPhoto(source) {
     console.log("getPhoto");
-  // Retrieve image file location from specified source
-  navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 50, 
-    destinationType: Camera.DestinationType.FILE_URI,
-    sourceType: source });
+    navigator.camera.getPicture(onPhotoURISuccess, onFail, {
+        quality : 50,
+        destinationType : Camera.DestinationType.FILE_URI,
+        sourceType : source
+    });
 }
 
-// Called if something bad happens.
-// 
 function onFail(message) {
-  // alert('Failed because: ' + message);
-  conole.log('Failed because: ' + message);
+    conole.log('Failed because: ' + message);
 }
