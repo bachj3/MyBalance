d6c35059b626dcb8cb6c5198e979dabaa2693aaa
bachj3 <bachj3@bfh.ch>
2014-06-03 11:08:16 +0200

    source

--- js/photo.js
+++ js/photo.js
@@ -1,62 +1,65 @@
-alert("photo.js");
-var pictureSource;   // picture source
-var destinationType; // sets the format of returned value 
+var pictureSource;
+// picture source
+var destinationType;
+// sets the format of returned value
 
 // Wait for PhoneGap to connect with the device
 //
-document.addEventListener("deviceready",onDeviceReady,false);
+document.addEventListener("deviceready", onDeviceReady, false);
 
 // PhoneGap is ready to be used!
 //
 function onDeviceReady() {
     alert("onDeviceReady");
     console.log("onDeviceReady");
-    pictureSource=navigator.camera.PictureSourceType;
-    destinationType=navigator.camera.DestinationType;
+    pictureSource = navigator.camera.PictureSourceType;
+    destinationType = navigator.camera.DestinationType;
+    alert(pictureSource);
+    alert(destinationType);
 }
 
 // Called when a photo is successfully retrieved
 //
 function onPhotoDataSuccess(imageData) {
     console.log("onPhotoDataSuccess");
-  // Uncomment to view the base64 encoded image data
-  // console.log(imageData);
-  // alert(imageData);
+    // Uncomment to view the base64 encoded image data
+    // console.log(imageData);
+    // alert(imageData);
 
-  // Get image handle
-  //
-  var img_selfie = document.getElementById('e_ref');
+    // Get image handle
+    //
+    var img_selfie = document.getElementById('e_ref');
 
-  // Unhide image elements
-  //
-  img_selfie.style.display = 'block';
+    // Unhide image elements
+    //
+    img_selfie.style.display = 'block';
 
-  // Show the captured photo
-  // The inline CSS rules are used to resize the image
-  //
-  img_selfie.src = "data:image/jpeg;base64," + imageData;
+    // Show the captured photo
+    // The inline CSS rules are used to resize the image
+    //
+    img_selfie.src = "data:image/jpeg;base64," + imageData;
 }
 
 // Called when a photo is successfully retrieved
 //
 function onPhotoURISuccess(imageURI) {
     console.log("onPhotoURISuccess");
-  // Uncomment to view the image file URI 
-  // console.log(imageURI);
-  // alert(imageURI);
+    // Uncomment to view the image file URI
+    // console.log(imageURI);
+    // alert(imageURI);
 
-  // Get image handle
-  //
-  var img_selfie = document.getElementById('e_ref');
+    // Get image handle
+    //
+    var img_selfie = document.getElementById('e_ref');
 
-  // Unhide image elements
-  //
-  img_selfie.style.display = 'block';
+    // Unhide image elements
+    //
+    img_selfie.style.display = 'block';
 
-  // Show the captured photo
-  // The inline CSS rules are used to resize the image
-  //
-  img_selfie.src = imageURI;
+    // Show the captured photo
+    // The inline CSS rules are used to resize the image
+    //
+    img_selfie.src = imageURI;
 
 }
 
@@ -64,31 +67,36 @@ function onPhotoURISuccess(imageURI) {
 //
 function capturePhoto() {
     console.log("capturePhoto");
-  // Take picture using device camera and retrieve image as base64-encoded string
-  navigator.camera.getPicture(onPhotoDataSuccess, onFail, {destinationType: Camera.DestinationType.FILE_URI, quality: 50 });
+    // Take picture using device camera and retrieve image as base64-encoded string
+    navigator.camera.getPicture(onPhotoDataSuccess, onFail, {
+        destinationType : Camera.DestinationType.FILE_URI,
+        quality : 50
+    });
 }
 
 // A button will call this function
 //
 // function capturePhotoEdit() {
-    // console.log("capturePhotoEdit");
-  // // Take picture using device camera, allow edit, and retrieve image as base64-encoded string  
-  // navigator.camera.getPicture(onPhotoDataSuccess, onFail, { destinationType: Camera.DestinationType.FILE_URI,quality: 20, allowEdit: true }); 
+// console.log("capturePhotoEdit");
+// // Take picture using device camera, allow edit, and retrieve image as base64-encoded string
+// navigator.camera.getPicture(onPhotoDataSuccess, onFail, { destinationType: Camera.DestinationType.FILE_URI,quality: 20, allowEdit: true });
 // }
 
 // A button will call this function
 //
 function getPhoto(source) {
     console.log("getPhoto");
-  // Retrieve image file location from specified source
-  navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 50, 
-    destinationType: Camera.DestinationType.FILE_URI,
-    sourceType: source });
+    // Retrieve image file location from specified source
+    navigator.camera.getPicture(onPhotoURISuccess, onFail, {
+        quality : 50,
+        destinationType : Camera.DestinationType.FILE_URI,
+        sourceType : source
+    });
 }
 
 // Called if something bad happens.
-// 
+//
 function onFail(message) {
-  alert('Failed because: ' + message);
-  conole.log('Failed because: ' + message);
+    alert('Failed because: ' + message);
+    conole.log('Failed because: ' + message);
 }
